b61b6be89c9278efdf1888f7b41dc6444b42dba5
Varun kasana <vkasana_be24@thapar.edu>
2025-03-18 16:07:35 +0000

    datatype changes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // we are using node js not browser
+
+let name = "varun"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+// null
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "varun");//tell the type of variable
+
+console.log(typeof null);
