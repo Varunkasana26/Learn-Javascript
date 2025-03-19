cdcdbdb3a107131fee9924e3042398a7fdb9beb1
Varun kasana <vkasana_be24@thapar.edu>
2025-03-19 13:13:17 +0000

    Comparsion

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2>=1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === strict check , not only value but also check datatype
+
+console.log("2"===2);//false
+console.log("2"==2);//true
