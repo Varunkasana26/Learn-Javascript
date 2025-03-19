7b84a0608cdbf7cb90d52c4ac26666be266edea2
Varun kasana <vkasana_be24@thapar.edu>
2025-03-19 15:42:06 +0000

    Data type summary

---
+++ 01_basics/Datatype-summary.js
@@ -0,0 +1,39 @@
+//particularly aiming for any interview
+
+// Primitive datatype 
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = "100";
+const scoreValue = 100.3;
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+const id = Symbol('123');
+const anotherId = Symbol('123');
+//console.log(id === anotherId);//false
+const bigNumber = 12345678987n
+// console.log(typeof bigNumber)
+// console.log(bigNumber)
+
+// javascript is dynamically type language not statically typed
+// which mean you dont need to declare type of variable
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["captain america", "spider man", "iron man"]
+let myObj = {
+    name: "Varun",
+    age: 18,
+}
+
+const myFunction = function(){
+    console.log("Hellor world");
+}
+
+console.log(typeof outsideTemp)//object
+console.log(typeof myFunction)//give result fuction but it's said to be object function
+
+
