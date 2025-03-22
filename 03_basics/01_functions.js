d8bb9b61ee2bee3633167bef1d5e41b35396819a
Varun kasana <vkasana_be24@thapar.edu>
2025-03-22 09:33:05 +0000

    fnx done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -24,4 +24,43 @@ function addTwonumbers(number1, number2){
 }
 
 const result = addTwonumbers(3,5)
-console.log("Result: ",result);
+//console.log("Result: ",result);
+
+function loginUserMessage(username){
+    if(username === undefined){//can also write !username
+        console.log("Please enter a valid username")
+        return 
+    }
+    return`${username} just logged in`
+}
+
+//console.log(loginUserMessage("Varun"))
+//console.log(loginUserMessage())
+
+
+//rest operator / spread operator
+function calculateCartprice(...num1){
+    return num1
+}
+
+console.log(calculateCartprice(200,400,500))
+
+const user = {
+    username: "Varun",
+    price: 200
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+handleObject(user)
+
+const myNewArray = [200,400,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 600]));
