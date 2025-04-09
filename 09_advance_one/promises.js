ca35f3cb1b62026ac2d97e8ce433fcbbd8da2a8d
Varun kasana <vkasana_be24@thapar.edu>
2025-04-09 02:24:00 +0000

    Promises

---
+++ 09_advance_one/promises.js
@@ -0,0 +1,96 @@
+//fetch('https://something.com').then().catch().finally()
+
+const promiseOne = new Promise(function(resolve, reject){
+    //Do an async task
+    // DB calls, cryptography, network
+    setTimeout(function(){
+        console.log('Async task is complete');
+        resolve()
+    },1000)
+})
+
+
+promiseOne.then(function(){
+    console.log("Promise consumed");
+})
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task 2");
+        resolve()
+    },1000)
+}).then(function(){
+    console.log('Async 2 resolved')
+})
+
+const promisethree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username: "varun", email: "vk@mail.com"})
+    },1000)
+})
+
+promisethree.then(function(user){
+    console.log(user);
+})
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if(!error){
+            resolve({username: "varun123", password: "1234"})
+        } else{
+            reject('Error : something went wrong')
+        }
+    })
+})
+
+promiseFour.then((user) => {
+    console.log(user);
+    return user.username
+}).then((username) => {
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
+}).finally(() => console.log('The promise is either resolved or rejected'
+))
+
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if(!error){
+            resolve({username: "Javascript", password: "123"})
+        }else{
+            reject('ERROR: JS WENT WRONG')
+        }
+    },1000)
+});
+
+async function consumePromiseFive(){
+    try{
+        const response = await promiseFive
+    console.log(response);
+    } catch(error){
+        console.log(error);
+    }
+}
+consumePromiseFive()
+
+// async function getalluser(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+// }
+// getalluser
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data);   
+})
+.catch((error)=> console.log(error))
